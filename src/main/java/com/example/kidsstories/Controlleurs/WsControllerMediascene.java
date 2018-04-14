25b6133104be61682f3eaed8a949e6bf68d1cafd
manel <hamdi.manelmh@gmail.com>
2018-04-14 03:06:44 +0100

    listMediascene WebS

---
+++ src/main/java/com/example/kidsstories/Controlleurs/WsControllerMediascene.java
@@ -0,0 +1,29 @@
+package com.example.kidsstories.Controlleurs;
+
+import com.example.kidsstories.Entities.Mediascene;
+import com.example.kidsstories.ModelInterfaces.IMediasceneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Mediascene")
+public class WsControllerMediascene {
+
+    @Autowired
+    private IMediasceneService iMediasceneService;
+
+    @RequestMapping(value = "/allMs/{idConte}", method = RequestMethod.GET, produces = "application/json")
+    public List<Mediascene> lstConte(@PathVariable("idConte") int idConte) {
+        try {
+            return iMediasceneService.ListMs(idConte);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
